bfa6aa4f66a8e96ea30fa4d5bd9a9faae4e92b1a
gwroth89 <greg@gregs-mbp.lan>
2022-10-17 11:43:20 -0700

    PyBank and PyPoll Complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,7 @@ csvReader = csv.reader(electionData, delimiter = ",")
 totalVotes = 0
 candidate = []
 candidateList = []
-candidateVoteList = []
+candidateVoteList = {}
 outputFile = "pollanalysis.txt"
 chuckVotes = 0
 dianaVotes = 0
@@ -32,26 +32,30 @@ with open(csvPath, 'r') as csvfile:
         chuckVotes = chuckVotes + candidate.count('Charles Casper Stockham')
         dianaVotes = dianaVotes + candidate.count('Diana DeGette')
         rayVotes = rayVotes + candidate.count('Raymon Anthony Doane')
-        # calculate each candidates votes share
-        chuckPercent = chuckVotes / totalVotes
-        dianaPercent = dianaVotes / totalVotes
-        rayPercent = rayVotes / totalVotes
-        # # identify the winner
-        # candidateVoteList = [chuckVotes, dianaVotes, rayVotes]
-        # winnerVoteCount = max(candidateVoteList)
+        # calculate each candidates vote share
+        chuckPercent = round(100 * (chuckVotes / totalVotes), 2)
+        dianaPercent = round(100 * (dianaVotes / totalVotes), 2)
+        rayPercent = round(100 * (rayVotes / totalVotes), 2)
+    # identify the winner
+    candidateVoteList = {
+        'Charles Casper Stockham': chuckVotes, 
+        'Diana DeGette' : dianaVotes, 
+        'Raymon Anthony Doane' : rayVotes
+        }
+    candidateKey = max(candidateVoteList, key=candidateVoteList.get)
 
 
 # store and format output
 output = (
     f"Total Votes: {totalVotes}\n"
-    f"Charles Casper Stockham - Votes: {chuckVotes} Vote %: {chuckPercent}\n"
-    f"Diana DeGette - Votes: {dianaVotes} Vote %: {dianaPercent}\n"
-    f"Raymon Anthony Doane - Votes: {rayVotes} Vote %: {rayPercent}\n"
-    #f"Winner: {profitLossMin} {minMonth}\n"
+    f"Charles Casper Stockham - Votes: {chuckVotes} Vote %: {chuckPercent}%\n"
+    f"Diana DeGette - Votes: {dianaVotes} Vote %: {dianaPercent}%\n"
+    f"Raymon Anthony Doane - Votes: {rayVotes} Vote %: {rayPercent}%\n"
+    f"Winner: {candidateKey}\n"
 )
 
 # print to console and output to txt file
 print(output)
 
-# with open(outputFile, 'w') as txtFile:
-#      txtFile.write(output)
+with open(outputFile, 'w') as txtFile:
+     txtFile.write(output)
